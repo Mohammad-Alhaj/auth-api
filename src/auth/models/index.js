19f5697e7a41cbf51d77bb88d2f9181425a4266d
Mohammad Alhaj <mohammadbasil.alhaj@gmail.com>
2022-06-19 17:50:54 +0300

    00

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -8,15 +8,16 @@ const { Sequelize,DataTypes } = require('sequelize');
 
  const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DATABASE_URL;
 
- const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
-   dialectOptions: {
-     ssl: {
-       require: true,
-       rejectUnauthorized: false,
-     }
-   }
- } : {};
- const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
+const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
+} : {};
+
+const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
 
 
 const Food = food(sequelize, DataTypes);
