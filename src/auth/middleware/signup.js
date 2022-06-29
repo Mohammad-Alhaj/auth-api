b5a79d4e4387725eaba491e5a027ec14135e42d2
Mohammad Alhaj <mohammadbasil.alhaj@gmail.com>
2022-06-29 15:37:11 +0300

    handler errors (bearer,basic)

--- src/auth/middleware/signup.js
+++ src/auth/middleware/signup.js
@@ -4,17 +4,16 @@ const User = require('../models/users-model')
 const bcrypt = require("bcrypt");
 
 const signup = (async(req,res,next)=>{
-    const username = req.body.username;
-    const role = req.body.role
-    const password = await bcrypt.hash(req.body.password, 10);
-    const record = await User.create({ 
-        username: username,
-         password: password,
-         role:role
-         });
-  res.status(201).json(record);
-
-next()
+    try{
+      const username = req.body.username;
+      const password = await bcrypt.hash(req.body.password, 10);
+      const record = await User.create({ username: username, password: password });
+    res.status(201).json(record);
+  
+  next()
+}catch(err){
+  next(err)
+}
 })
 
 module.exports = signup
